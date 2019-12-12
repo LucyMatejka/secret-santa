30e11d469b0757017e74a30954316ca275641f4e
Lucy Matejka <lucymatejka@yahoo.co.uk>
2019-12-12 17:41:03 +0000

    create view files

--- secret-santa/app.rb
+++ secret-santa/app.rb
@@ -3,7 +3,11 @@ require 'sinatra/base'
 class SecretSanta < Sinatra::Base
   get '/' do
     'Secret Santa'
-#    erb: 'secretsanta/index'
+   erb :index
+  end
+
+  get "/secretsanta/new" do
+    erb :new
   end
 
   run! if app_file == $0
